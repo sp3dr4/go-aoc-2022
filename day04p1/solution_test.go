b5469d5f031fac683ee0a24780e2e8f1a1348b49
Simone Pedrazzi <simone@mirta.com>
2023-10-02 17:47:49 +0200

    day4

---
+++ day04p1/solution_test.go
@@ -0,0 +1,38 @@
+package day04p1
+
+import (
+	"strings"
+	"testing"
+
+	"aoc/utils"
+)
+
+var testInput = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+func TestSolve(t *testing.T) {
+	tests := []struct {
+		input  string
+		answer int
+	}{
+		{testInput, 2},
+	}
+
+	if testing.Verbose() {
+		utils.Verbose = true
+	}
+
+	for _, test := range tests {
+		r := strings.NewReader(test.input)
+
+		result := Solve(r).(int)
+
+		if result != test.answer {
+			t.Errorf("Expected %d, got %d", test.answer, result)
+		}
+	}
+}
---
+++ day04p2/solution.go
@@ -0,0 +1,53 @@
+package day04p2
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"aoc/utils"
+)
+
+func Solve(r io.Reader) any {
+	lines := utils.ReadLines(r)
+
+	res := 0
+	for _, v := range lines {
+		pair := strings.Split(v, ",")
+
+		elf1Lower, elf1Upper, err := bounds(pair[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+
+		elf2Lower, elf2Upper, err := bounds(pair[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+
+		if elf1Lower <= elf2Lower && elf1Upper >= elf2Lower {
+			res++
+		} else if elf1Lower > elf2Lower && elf2Upper >= elf1Lower {
+			res++
+		}
+	}
+
+	return res
+}
+
+func bounds(v string) (int, int, error) {
+	bounds := strings.Split(v, "-")
+	lower, err := strconv.Atoi(bounds[0])
+	if err != nil {
+		return 0, 0, err
+	}
+	upper, err := strconv.Atoi(bounds[1])
+	if err != nil {
+		return 0, 0, err
+	}
+	return lower, upper, nil
+}
